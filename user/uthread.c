48cf4a4fd1fbf2722a2719209acfb8fc7dc3db64
WalleDong <747196076@qq.com>
2022-05-29 16:17:18 +0800

    Uthread: switching between threads

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,51 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// 用户线程的上下文结构体
+struct tcontext {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  char       name[16];          /* thread name (debugging) */
+  struct tcontext context;      /* 用户进程上下文 */
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
+
+char*
+safestrcpy(char *s, const char *t, int n)
+{
+  char *os;
+
+  os = s;
+  if(n <= 0)
+    return os;
+  while(--n > 0 && (*s++ = *t++) != 0)
+    ;
+  *s = 0;
+  return os;
+}
+       
 void 
 thread_init(void)
 {
@@ -30,6 +65,7 @@ thread_init(void)
   // a RUNNABLE thread.
   current_thread = &all_thread[0];
   current_thread->state = RUNNING;
+  safestrcpy(current_thread->name, "main", sizeof(current_thread->name));
 }
 
 void 
@@ -50,6 +86,13 @@ thread_schedule(void)
     t = t + 1;
   }
 
+  // thread_a b c 函数中的内容都执行完毕后
+  // 会将 current_thread->state = FREE;
+  // 因此，thread_a b c 函数中最后一次调用thread_schedule
+  // 线程池中的线程都是free，所以 next_thread = 0
+  // 所以 最最后只会打印 一条 “thread_schedule: no runnable threads”
+  // 整个程序在main函数的thread_schedule(); 就退出了
+  // 不会走到 exit(0)
   if (next_thread == 0) {
     printf("thread_schedule: no runnable threads\n");
     exit(-1);
@@ -63,20 +106,24 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->context, (uint64)&current_thread->context);
   } else
     next_thread = 0;
 }
 
 void 
-thread_create(void (*func)())
+thread_create(void (*func)(), const char *thread_name)
 {
   struct thread *t;
-
+  // 找到一个空闲的线程
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)func;                   // 设定函数返回地址
+  t->context.sp = (uint64)t->stack + STACK_SIZE;  // 设定栈指针
+  safestrcpy(t->name, thread_name, sizeof(t->name));
 }
 
 void 
@@ -94,8 +141,14 @@ thread_a(void)
 {
   int i;
   printf("thread_a started\n");
-  a_started = 1;
+  a_started = 1;  // 标记 thread_a 已经准备好开始执行了
   while(b_started == 0 || c_started == 0)
+    // 但是检查了下另外两个线程，还没有准备好
+    // 因此 thread_yield 把当前 thread_a 挂起，让出cpu的执行权
+    // 再进入 thread_schedule，这个时候 current_thread 还是 a
+    // 通过 t = current_thread + 1; next_thread = t;
+    // 把 线程b 作为next_thread
+    // 再之后就把就执行到 线程b里面
     thread_yield();
   
   for (i = 0; i < 100; i++) {
@@ -116,6 +169,7 @@ thread_b(void)
   printf("thread_b started\n");
   b_started = 1;
   while(a_started == 0 || c_started == 0)
+    // 同样的处理，转到 线程c
     thread_yield();
   
   for (i = 0; i < 100; i++) {
@@ -137,7 +191,8 @@ thread_c(void)
   c_started = 1;
   while(a_started == 0 || b_started == 0)
     thread_yield();
-  
+  // 此时所有的线程都准备好执行，因此先打印的是 thread_c 0
+  // c_n 每增加一 就出让cpu
   for (i = 0; i < 100; i++) {
     printf("thread_c %d\n", i);
     c_n += 1;
@@ -155,9 +210,16 @@ main(int argc, char *argv[])
   a_started = b_started = c_started = 0;
   a_n = b_n = c_n = 0;
   thread_init();
-  thread_create(thread_a);
-  thread_create(thread_b);
-  thread_create(thread_c);
+  thread_create(thread_a, "thread_a");
+  thread_create(thread_b, "thread_b");
+  thread_create(thread_c, "thread_c");
   thread_schedule();
+  /* 第一次触发 thread_schedule 时
+  会检查线程池 all_thread 中哪个线程是RUNNABLE状态
+  这时候先检查出来是 thread_a
+  把 main 线程的寄存器信息保存在 main线程的context里面
+  再把 thread_a 线程的context信息恢复到寄存器里面
+  因此开始执行 thread_a 中的内容，第一行打印的是 thread_a started
+  */
   exit(0);
 }
