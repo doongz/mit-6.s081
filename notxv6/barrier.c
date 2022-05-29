d746c51076010c96519fa571cd9a55dbefc395cb
WalleDong <747196076@qq.com>
2022-05-29 18:13:34 +0800

    Barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -25,12 +25,22 @@ barrier_init(void)
 static void 
 barrier()
 {
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+  // 申请持有锁
+  pthread_mutex_lock(&bstate.barrier_mutex);
+
+  bstate.nthread++;
+  if(bstate.nthread == nthread) {
+    // 所有线程已到达
+    bstate.round++;
+    bstate.nthread = 0;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    // 等待其他线程
+    // 调用pthread_cond_wait时，mutex必须已经持有
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  // 释放锁
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
